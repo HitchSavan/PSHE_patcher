404b4b1eea8e7ba1ffcd3573c5738d3a854aa836
Daniil Temnyj <temnyy1@gmail.com>
2024-02-26 13:02:19 +0300

    Add on-run message

--- src/main/java/patcher/PatcherMain.java
+++ src/main/java/patcher/PatcherMain.java
@@ -2,6 +2,8 @@ package patcher;
 public class PatcherMain {
 
     public static void main(String[] args) {
-        
+        System.out.println("""
+                This jar lib is not meant to be executed as standalone app.\n
+                Use native courgette for terminal usage or user client for UI.""");
     }
 }
