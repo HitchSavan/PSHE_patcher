3cb6921335a2767e885605036489f1d155ac24a6
Daniil Temnyj <temnyy1@gmail.com>
2024-03-04 15:03:08 +0300

    Implemented GET, PUT, POST, DELETE (needs tests, unhandled exceptions)

--- src/main/java/patcher/remote_api/endpoints/Versions.java
+++ src/main/java/patcher/remote_api/endpoints/Versions.java
@@ -16,8 +16,8 @@ public class Versions {
     public static JSONObject getVersions(Map<String, String> params) throws IOException {
         return Connector.connect(baseEndpoint, Connector.Methods.GET, params);
     }
-    public static int postVersions(Map<String, String> params) throws IOException {
-        Connector.connect(baseEndpoint, Connector.Methods.POST, params);
+    public static int postVersions(JSONObject data, Map<String, String> params) throws IOException {
+        Connector.connect(baseEndpoint, Connector.Methods.POST, params, data);
         return Connector.getResponseCode();
     }
     public static int deleteVersions(Map<String, String> params) throws IOException {
@@ -27,16 +27,16 @@ public class Versions {
     public static JSONObject getHistory(Map<String, String> params) throws IOException {
         return Connector.connect(historyEndpoint, Connector.Methods.GET, params);
     }
-    public static int putRoot(Map<String, String> params) throws IOException {
-        Connector.connect(rootEndpoint, Connector.Methods.PUT, params);
+    public static int putRoot(JSONObject data, Map<String, String> params) throws IOException {
+        Connector.connect(rootEndpoint, Connector.Methods.PUT, params, data);
         return Connector.getResponseCode();
     }
     public static JSONObject getSwitch(Map<String, String> params) throws IOException {
         return Connector.connect(switchEndpoint, Connector.Methods.GET, params);
     }
 
-    public static int postVersions() throws IOException {
-        return postVersions(null);
+    public static int postVersions(JSONObject data) throws IOException {
+        return postVersions(data, null);
     }
     public static int deleteVersions() throws IOException {
         return deleteVersions(null);
@@ -44,8 +44,8 @@ public class Versions {
     public static JSONObject getHistory() throws IOException {
         return getHistory(null);
     }
-    public static int putRoot() throws IOException {
-        return putRoot(null);
+    public static int putRoot(JSONObject data) throws IOException {
+        return putRoot(data, null);
     }
     public static JSONObject getVersions() throws IOException {
         return getVersions(null);
--- src/main/java/patcher/remote_api/utils/Connector.java
+++ src/main/java/patcher/remote_api/utils/Connector.java
@@ -1,8 +1,10 @@
 package patcher.remote_api.utils;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
@@ -36,7 +38,7 @@ public class Connector {
     @Getter @Setter
     private static int responseCode;
 
-    public static JSONObject connect(String endpointUrl, Methods method, Map<String, String> parameters) throws IOException {
+    public static JSONObject connect(String endpointUrl, Methods method, Map<String, String> parameters, JSONObject data) throws IOException {
         endpointUrl = getBaseUrl() + endpointUrl;
         URL url = parameters != null && !parameters.isEmpty() ?
                 ParamsBuilder.getUrlWithParams(endpointUrl, parameters) : new URL(endpointUrl);
@@ -48,44 +50,94 @@ public class Connector {
 
         JSONObject response = new JSONObject();
 
-        switch (method) {
-            case GET:
-                StringBuffer content = new StringBuffer();
-                if (responseCode > 299) {
-                    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                    String inputLine;
-                    while ((inputLine = in.readLine()) != null) {
-                        content.append(inputLine);
-                    }
-                    in.close();
-                } else {
-                    content.append(new JSONObject().put("error", responseCode));
-                }
-                response = new JSONObject(content.toString());
-                break;
-        
-            case PUT:
-            
-                break;
-        
-            case POST:
-            
-                break;
-        
-            case DELETE:
-            
-                break;
-        
-            default:
-                break;
+        if (responseCode < 300) {
+            switch (method) {
+                case GET:
+                    response = get(con);
+                    break;
+
+                case PUT:
+                    put(con, data);
+                    break;
+
+                case POST:
+                    response = post(con, data);
+                    break;
+
+                default:
+                    break;
+            }
+            response.put("success", true);
+        } else {
+            response.put("success", false);
         }
+
+        response.put("status", responseCode);
+        response.put("message", con.getResponseMessage());
 
         con.disconnect();
 
         return response;
     }
-
     public static JSONObject connect(String endpointUrl, Methods method) throws IOException {
-        return connect(endpointUrl, method, null);
+        return connect(endpointUrl, method, null, null);
+    }
+    public static JSONObject connect(String endpointUrl, Methods method, Map<String, String> parameters) throws IOException {
+        return connect(endpointUrl, method, parameters, null);
+    }
+    public static JSONObject connect(String endpointUrl, Methods method, JSONObject data) throws IOException {
+        return connect(endpointUrl, method, null, data);
+    }
+
+    private static JSONObject get(HttpURLConnection con) throws IOException {
+        StringBuffer content = new StringBuffer();
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+
+        return new JSONObject(content.toString());
+    }
+
+    private static void put(HttpURLConnection con, JSONObject data) {
+        DataOutputStream dataOutputStream = null;
+        try {
+            con.setDoOutput(true);
+            dataOutputStream = new DataOutputStream(con.getOutputStream());
+            dataOutputStream.write(data.toString().getBytes());
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }  finally {
+            if (dataOutputStream != null) {
+                try {
+                    dataOutputStream.flush();
+                    dataOutputStream.close();
+                } catch (IOException exception) {
+                    exception.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static JSONObject post(HttpURLConnection con, JSONObject data) throws IOException {
+        con.setDoInput(true);
+        con.setDoOutput(true);
+
+        try(OutputStream os = con.getOutputStream()) {
+            byte[] input = data.toString().getBytes("utf-8");
+            os.write(input, 0, input.length);			
+        }
+
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+            return new JSONObject(response.toString());
+        }
     }
 }
