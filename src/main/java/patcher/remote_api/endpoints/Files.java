141ea6bbffcc0d5377fc85cf120ecbf3b40b6f57
Daniil Temnyj <temnyy1@gmail.com>
2024-03-04 15:48:13 +0300

    Implemented requests for endpoints (untested)

--- src/main/java/patcher/remote_api/endpoints/Files.java
+++ src/main/java/patcher/remote_api/endpoints/Files.java
@@ -1,5 +1,73 @@
 package patcher.remote_api.endpoints;
 
+import java.io.IOException;
+import java.util.Map;
+
+import org.json.JSONObject;
+
+import patcher.remote_api.utils.Connector;
+
 public class Files {
-    
+    private static final String baseEndpoint = "/files";
+    private static final String allFilesEndpoint = baseEndpoint + "/all";
+    private static final String historyEndpoint = baseEndpoint + "/history";
+    private static final String versionEndpoint = baseEndpoint + "/version";
+    private static final String rootEndpoint = baseEndpoint + "/root";
+
+    public static JSONObject getFiles(Map<String, String> params) throws IOException {
+        return Connector.connect(baseEndpoint, Connector.Methods.GET, params);
+    }
+    public static int deleteFiles(Map<String, String> params) throws IOException {
+        Connector.connect(baseEndpoint, Connector.Methods.DELETE, params);
+        return Connector.getResponseCode();
+    }
+    public static JSONObject getAll() throws IOException {
+        return Connector.connect(allFilesEndpoint, Connector.Methods.GET);
+    }
+    public static JSONObject getHistory(Map<String, String> params) throws IOException {
+        return Connector.connect(historyEndpoint, Connector.Methods.GET, params);
+    }
+    public static JSONObject getRoot(Map<String, String> params) throws IOException {
+        return Connector.connect(rootEndpoint, Connector.Methods.GET, params);
+    }
+    public static int postRoot(JSONObject data, Map<String, String> params) throws IOException {
+        Connector.connect(rootEndpoint, Connector.Methods.POST, params, data);
+        return Connector.getResponseCode();
+    }
+    public static JSONObject getVersion(Map<String, String> params) throws IOException {
+        return Connector.connect(versionEndpoint, Connector.Methods.GET, params);
+    }
+    public static int postVersion(JSONObject data, Map<String, String> params) throws IOException {
+        Connector.connect(versionEndpoint, Connector.Methods.POST, params, data);
+        return Connector.getResponseCode();
+    }
+    public static int deleteVersion(Map<String, String> params) throws IOException {
+        Connector.connect(versionEndpoint, Connector.Methods.DELETE, params);
+        return Connector.getResponseCode();
+    }
+
+    public static JSONObject getFiles() throws IOException {
+        return getFiles(null);
+    }
+    public static int deleteFiles() throws IOException {
+        return deleteFiles();
+    }
+    public static JSONObject getHistory() throws IOException {
+        return getHistory(null);
+    }
+    public static JSONObject getRoot() throws IOException {
+        return getRoot(null);
+    }
+    public static int postRoot(JSONObject data) throws IOException {
+        return postRoot(data, null);
+    }
+    public static JSONObject getVersion() throws IOException {
+        return getVersion(null);
+    }
+    public static int postVersion(JSONObject data) throws IOException {
+        return postVersion(data);
+    }
+    public static int deleteVersion() throws IOException {
+        return deleteVersion(null);
+    }
 }
--- src/main/java/patcher/remote_api/endpoints/Patches.java
+++ src/main/java/patcher/remote_api/endpoints/Patches.java
@@ -1,5 +1,53 @@
 package patcher.remote_api.endpoints;
 
+import java.io.IOException;
+import java.util.Map;
+
+import org.json.JSONObject;
+
+import patcher.remote_api.utils.Connector;
+
 public class Patches {
-    
+    private static final String baseEndpoint = "/patches";
+    private static final String allPatchesEndpoint = baseEndpoint + "/all";
+    private static final String fileEndpoint = baseEndpoint + "/file";
+    private static final String infoEndpoint = fileEndpoint + "/info";
+
+    public static int postPatches(JSONObject data, Map<String, String> params) throws IOException {
+        Connector.connect(baseEndpoint, Connector.Methods.POST, params, data);
+        return Connector.getResponseCode();
+    }
+    public static int deletePatches(Map<String, String> params) throws IOException {
+        Connector.connect(baseEndpoint, Connector.Methods.DELETE, params);
+        return Connector.getResponseCode();
+    }
+    public static JSONObject getAll() throws IOException {
+        return Connector.connect(allPatchesEndpoint, Connector.Methods.GET);
+    }
+    public static JSONObject getFile(Map<String, String> params) throws IOException {
+        return Connector.connect(fileEndpoint, Connector.Methods.GET, params);
+    }
+    public static int postFile(JSONObject data, Map<String, String> params) throws IOException {
+        Connector.connect(fileEndpoint, Connector.Methods.POST, params, data);
+        return Connector.getResponseCode();
+    }
+    public static JSONObject getInfo(Map<String, String> params) throws IOException {
+        return Connector.connect(infoEndpoint, Connector.Methods.GET, params);
+    }
+
+    public static int postPatches(JSONObject data) throws IOException {
+        return postPatches(data, null);
+    }
+    public static int deletePatches() throws IOException {
+        return deletePatches();
+    }
+    public static JSONObject getFile() throws IOException {
+        return getFile(null);
+    }
+    public static int postFile(JSONObject data) throws IOException {
+        return postFile(data, null);
+    }
+    public static JSONObject getInfo() throws IOException {
+        return getInfo(null);
+    }
 }
