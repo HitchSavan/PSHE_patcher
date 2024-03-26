d7f4ca6a79f292b52b26e4b9bf2e60fe0d838670
Daniil Temnyj <temnyy1@gmail.com>
2024-03-26 12:25:38 +0300

    Added patching handeling in case working directory is patching file parent

--- src/main/java/patcher/utils/patching_utils/Patcher.java
+++ src/main/java/patcher/utils/patching_utils/Patcher.java
@@ -7,54 +7,93 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.List;
 
+import patcher.utils.files_utils.Directories;
+
 public class Patcher {
     public static void generatePatch(Path oldAbsoluteFile, Path newAbsoluteFile, Path patchAbsoluteFile,
             Path courgetteWorkingDirectory, boolean redirectOutput) throws IOException, InterruptedException {
-        initFiles(oldAbsoluteFile, newAbsoluteFile);
-        Path oldMovedFile = courgetteWorkingDirectory.resolve(Paths.get("courgette_old", oldAbsoluteFile.getFileName().toString()));
-        Path newMovedFile = courgetteWorkingDirectory.resolve(Paths.get("courgette_new", newAbsoluteFile.getFileName().toString()));
-        Path patchMovedFile = courgetteWorkingDirectory.resolve(Paths.get("courgette_patch", patchAbsoluteFile.getFileName().toString()));
 
-        Files.copy(oldAbsoluteFile, oldMovedFile, StandardCopyOption.REPLACE_EXISTING);
-        Files.copy(newAbsoluteFile, newMovedFile, StandardCopyOption.REPLACE_EXISTING);
+        initFiles(oldAbsoluteFile, newAbsoluteFile);
+        boolean workInOldPlace = courgetteWorkingDirectory.equals(oldAbsoluteFile.getParent());
+        boolean workInNewPlace = courgetteWorkingDirectory.equals(newAbsoluteFile.getParent());
+
+        Path oldRelativeFile;
+        Path newRelativeFile;
+        Path patchRelativeFile;
+        if (workInOldPlace) {
+            oldRelativeFile = oldAbsoluteFile;
+        } else {
+            oldRelativeFile = courgetteWorkingDirectory.resolve(Paths.get("courgette_files", "old", oldAbsoluteFile.getFileName().toString()));
+        }
+        if (workInNewPlace) {
+            newRelativeFile = newAbsoluteFile;
+        } else {
+            newRelativeFile = courgetteWorkingDirectory.resolve(Paths.get("courgette_files", "new", newAbsoluteFile.getFileName().toString()));
+        }
+        patchRelativeFile = courgetteWorkingDirectory.resolve(Paths.get("courgette_files", "patch", patchAbsoluteFile.getFileName().toString()));
+
+        List.of(oldRelativeFile, newRelativeFile, patchRelativeFile).forEach(file -> {
+            file.getParent().toFile().mkdirs();
+        });
+
+        Files.copy(oldAbsoluteFile, oldRelativeFile, StandardCopyOption.REPLACE_EXISTING);
+        Files.copy(newAbsoluteFile, newRelativeFile, StandardCopyOption.REPLACE_EXISTING);
 
         String[] args = {"-gen",
-                oldMovedFile.toString(),
-                newMovedFile.toString(),
-                patchMovedFile.toString()
+                courgetteWorkingDirectory.relativize(oldRelativeFile).toString(),
+                courgetteWorkingDirectory.relativize(newRelativeFile).toString(),
+                courgetteWorkingDirectory.relativize(patchRelativeFile).toString()
             };
         execute(args, courgetteWorkingDirectory, false, redirectOutput);
 
-        Files.move(patchMovedFile, patchAbsoluteFile, StandardCopyOption.REPLACE_EXISTING);
-        Files.delete(oldMovedFile);
-        Files.delete(newMovedFile);
+        Files.move(patchRelativeFile, patchAbsoluteFile, StandardCopyOption.REPLACE_EXISTING);
+
+        if (workInOldPlace || workInNewPlace) {
+            Directories.deleteDirectory(courgetteWorkingDirectory.resolve("courgette_files"));
+        } else {
+            Files.delete(oldRelativeFile);
+            Files.delete(newRelativeFile);
+        }
     }
 
     public static void applyPatch(Path oldAbsoluteFile, Path newAbsoluteFile, Path patchAbsoluteFile,
             Path courgetteWorkingDirectory, boolean replaceFiles, boolean redirectOutput) throws IOException, InterruptedException {
+
         initFiles(oldAbsoluteFile, newAbsoluteFile);
+        boolean workInplace = courgetteWorkingDirectory.equals(oldAbsoluteFile.getParent());
 
-        Path oldMovedFile = courgetteWorkingDirectory.resolve(Paths.get("courgette_files", "old", oldAbsoluteFile.getFileName().toString()));
-        Path newMovedFile = courgetteWorkingDirectory.resolve(Paths.get("courgette_files", "new", newAbsoluteFile.getFileName().toString()));
-        Path patchMovedFile = courgetteWorkingDirectory.resolve(Paths.get("courgette_files", "patch", patchAbsoluteFile.getFileName().toString()));
+        Path oldRelativeFile;
+        Path newRelativeFile;
+        Path patchRelativeFile;
+        if (workInplace) {
+            oldRelativeFile = oldAbsoluteFile;
+        } else {
+            oldRelativeFile = courgetteWorkingDirectory.resolve(Paths.get("courgette_files", "old", oldAbsoluteFile.getFileName().toString()));
+        }
+        newRelativeFile = courgetteWorkingDirectory.resolve(Paths.get("courgette_files", "new", newAbsoluteFile.getFileName().toString()));
+        patchRelativeFile = courgetteWorkingDirectory.resolve(Paths.get("courgette_files", "patch", patchAbsoluteFile.getFileName().toString()));
 
-        List.of(oldMovedFile, newMovedFile, patchMovedFile).forEach(file -> {
+        List.of(oldRelativeFile, newRelativeFile, patchRelativeFile).forEach(file -> {
             file.getParent().toFile().mkdirs();
         });
 
-        Files.copy(oldAbsoluteFile, oldMovedFile, StandardCopyOption.REPLACE_EXISTING);
-        Files.copy(patchAbsoluteFile, patchMovedFile, StandardCopyOption.REPLACE_EXISTING);
+        Files.copy(oldAbsoluteFile, oldRelativeFile, StandardCopyOption.REPLACE_EXISTING);
+        Files.copy(patchAbsoluteFile, patchRelativeFile, StandardCopyOption.REPLACE_EXISTING);
 
         String[] args = {"-apply",
-                oldMovedFile.toString(),
-                patchMovedFile.toString(),
-                newMovedFile.toString()
+                courgetteWorkingDirectory.relativize(oldRelativeFile).toString(),
+                courgetteWorkingDirectory.relativize(patchRelativeFile).toString(),
+                courgetteWorkingDirectory.relativize(newRelativeFile).toString()
             };
         execute(args, courgetteWorkingDirectory, replaceFiles, redirectOutput);
 
-        Files.move(newMovedFile, newAbsoluteFile, StandardCopyOption.REPLACE_EXISTING);
-        Files.delete(oldMovedFile);
-        Files.delete(patchMovedFile);
+        Files.move(newRelativeFile, newAbsoluteFile, StandardCopyOption.REPLACE_EXISTING);
+        if (workInplace) {
+            Directories.deleteDirectory(courgetteWorkingDirectory.resolve("courgette_files"));
+        } else {
+            Files.delete(oldRelativeFile);
+            Files.delete(patchRelativeFile);
+        }
     }
 
     private static void execute(String[] args, Path courgetteWorkingDirectory, boolean replaceFiles, boolean redirectOutput) throws IOException, InterruptedException {
